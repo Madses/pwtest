cb8117952a1e3aca311c98ca5c35c396adc403b7
Madses <k.l.m.aartsen@hotmail.com>
2021-10-08 11:50:08 +0200

    changes

--- src/App.js
+++ src/App.js
@@ -8,19 +8,22 @@ function App() {
     const [data, setData] = useState([]);
 
     useEffect(() => {
-        const requestOne = axios.get(
-            "https://www.alphavantage.co/query?function=OVERVIEW&symbol=IBM&apikey=I0R8K64CG3EHARV6"
-        );
-        const requestTwo = axios.get(
-            "https://www.alphavantage.co/query?function=OVERVIEW&symbol=MSFT&apikey=I0R8K64CG3EHARV6"
-        );
+        const requests = [
+            axios.get(
+                "https://www.alphavantage.co/query?function=OVERVIEW&symbol=IBM&apikey=I0R8K64CG3EHARV6"
+            ),
 
-        const requestThree = axios.get(
-            "https://www.alphavantage.co/query?function=OVERVIEW&symbol=AAPL&apikey=I0R8K64CG3EHARV6"
-        );
+            axios.get(
+                "https://www.alphavantage.co/query?function=OVERVIEW&symbol=MSFT&apikey=I0R8K64CG3EHARV6"
+            ),
+
+            axios.get(
+                "https://www.alphavantage.co/query?function=OVERVIEW&symbol=AAPL&apikey=I0R8K64CG3EHARV6"
+            ),
+        ];
 
         axios
-            .all([requestOne, requestTwo, requestThree])
+            .all(requests)
             .then(responses =>
                 setData(responses.map(response => response.data))
             )
@@ -29,7 +32,6 @@ function App() {
             });
     }, []);
 
-    console.log(data);
     return (
         <div className="mainContainer">
             <DataTable columns={columns} data={data} />
